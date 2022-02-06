23fed69d4493c386c8aa04a0efeb6a6e536f8e8b
Elijah <elijah.pelofske@student.nmt.edu>
2022-02-05 22:53:41 -0700

    testing out simple linear regression

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Apply machine learning to cryptocurrency data
 - python 3.9.1
 - Anaconda
 - pandas==1.4.0
+- Install cryptoCMD (`https://github.com/guptarohit/cryptoCMD`) can be installed using `pip install git+git://github.com/guptarohit/cryptoCMD.git`
+- scikit-learn==1.0.2
+- numpy==1.22.2
+- matplotlib==3.5.1
---
+++ test.py
@@ -0,0 +1,34 @@
+from cryptocmd import CmcScraper
+from sklearn.linear_model import LinearRegression
+import ast
+#from pmdarima import auto_arima
+import matplotlib.pyplot as plt
+import numpy as np
+
+cryptocurrency = "BTC"
+
+scraper = CmcScraper(cryptocurrency)
+json_data = ast.literal_eval(scraper.get_data("json"))
+
+json_data.reverse()
+
+data = []
+for a in json_data:
+	data.append(a["Open"])
+	data.append(a["Close"])
+
+plt.plot([a for a in range(len(data))], data, "-b.")
+plt.savefig("figures/"+cryptocurrency+".pdf")
+
+latest_datapoint = data[-1]
+del data[-1]
+
+lr = LinearRegression()
+lr.fit(np.array([a for a in range(len(data))]).reshape(-1, 1), data)
+lr_score = lr.score(np.array([a for a in range(len(data))]).reshape(-1, 1), data)
+print("Overall linear regression score:", lr_score)
+
+# Predicitng on one datapoint
+print("predicted datapoint: ", lr.predict(np.array([len(data)]).reshape(-1, 1)))
+
+print("Real datapoint:", latest_datapoint)
