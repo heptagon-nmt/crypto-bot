9fcd85bf97c03c01257b0025494d66e095ee3f7b
---- <56278570+1103s@users.noreply.github.com>
2022-03-09 02:22:22 -0700

    lets see if this example works

---
+++ doc_example.py
@@ -0,0 +1,18 @@
+"""
+The name of your module goes here.
+"""
+
+class ExapleClass():
+    """
+    A description of what your class does.
+    """
+    def example_function(self, text):
+        """
+        What your funciton does. EG: Prints text.
+
+        :arg text: The string to be printed.
+        :return: None
+        """
+        print(text)
+
+
