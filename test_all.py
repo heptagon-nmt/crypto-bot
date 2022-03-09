200f51cee7894ebc4a00cf7c41005683615fad6c
---- <56278570+1103s@users.noreply.github.com>
2022-03-09 04:52:26 -0700

    Added in unit tests

---
+++ test_all.py
@@ -0,0 +1,30 @@
+"""
+The unit test mecanism.
+"""
+import time
+import get_historical as gh
+
+assert len(gh.get_ids()) > 0, "Id list is malformed"
+
+assert gh.get_market_range(gh.get_ids()[0], time.time(),
+        time.time() - 60 * 60 * 24 * 5) is dict, "Query does not return json"
+
+assert gh.is_valid_id("    ") is False, "Invalid id was found"
+
+assert gh.is_valid_id(gh.get_ids()[0]) is True, "Valid id was not found"
+
+assert gh.get_n_days(gh.get_ids()[0], 1) is dict, "Query does not return json"
+
+import get_real_time as gr
+from keras.models import Sequential
+
+assert gr.get_data("ethereum") is dict, "Query does not return json"
+
+assert isinstance(gr.get_classifier(), Sequential), "Something is wrong?"
+
+from skforecast.model_selection import grid_search_forecaster
+import ML_predictor_backend as ml
+
+assert isinstance(ml.parameter_gridsearch,
+        grid_search_forecaster), "Something is wrong?"
+
