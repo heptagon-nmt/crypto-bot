6108d338a110e3c05e0962935dbf0309b50eb42d
---- <56278570+1103s@users.noreply.github.com>
2022-03-09 02:00:14 -0700

    forgot my autodoc

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ release = '0.0.1'
 # ones.
 extensions = [
         'sphinx.ext.githubpages',
-        'sphinx.ext.githubpages'
+        'sphinx.ext.autodoc'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
