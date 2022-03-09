754b459761690ab4506abf6ac1a234d6f5ca4a07
Elijah <elijah.pelofske@student.nmt.edu>
2022-03-09 13:12:58 -0700

    remove exit-zero

--- get_data.py
+++ get_data.py
@@ -9,6 +9,24 @@ import time
 
 url_prefix = "https://api.coingecko.com/api/v3/{}"
 
+from cryptocmd import CmcScraper
+import ast
+
+def pull_CMC_scraper_data(cryptocurrency_name):
+	"""
+	Query CMC Scraper API to get the cryptocurrency price data
+	"""
+	assert type(cryptocurrency_name) is str, "Cryptocurrency name must be a string"
+	scraper = CmcScraper(cryptocurrency_name)
+	json_data = ast.literal_eval(scraper.get_data("json"))
+	json_data.reverse()
+	data = []
+	for a in json_data:
+		data.append(a["Open"])
+		data.append(a["Close"])
+	return data
+
+
 def get_ids():
     with open("data/coin_list.json", "r") as f:
         data = json.load(f)
