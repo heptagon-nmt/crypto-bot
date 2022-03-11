dd5b2db62828fa9704fa64ca4e439dd5307e4d74
Elijah <elijah.pelofske@student.nmt.edu>
2022-03-11 15:05:03 -0700

    Add test for coingecko check valid id

--- test_all.py
+++ test_all.py
@@ -21,11 +21,7 @@ def test_default() -> None:
 	import get_data
 	price_data = get_data.pull_CMC_scraper_data("BTC")
 	assert type(price_data) is list
-	
-	#crypto_symbols_kraken = get_data.get_ids_kraken() This fails tests
-	#assert type(crypto_symbols_kraken) is list
-	#assert len(crypto_symbols_kraken) > 1
-	
-	#crypto_symbols_coingecko = get_data.get_ids_coingecko()
-	#assert type(crypto_symbols_coingecko) is list
-	#assert len(crypto_symbols_coingecko) > 1
+	assert len(price_data) > 1
+	check_valid = get_data.is_valid_id_coingecko("BTC")
+	assert check_valid == False
+
