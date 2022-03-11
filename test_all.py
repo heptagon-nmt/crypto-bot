d092fc8941e28378d9b905f71c9213736b0048d8
---- <56278570+1103s@users.noreply.github.com>
2022-03-10 20:14:16 -0700

    fixed tests

--- test_all.py
+++ test_all.py
@@ -1,12 +1,16 @@
 """
 The unit test mechanism.
 """
-import time
-#import get_data as gd
-#assert len(gd.get_ids()) > 0, "Id list is malformed"
+def test_default() -> None:
+    """
+    The default tests to be run.
+    """
+    import time
+    #import get_data as gd
+    #assert len(gd.get_ids()) > 0, "Id list is malformed"
 
-import ML_predictor_backend as ML
-pred = ML.predict_next_N_timesteps([1, 2, 3, 4, 5, 6], 2, 4, "linear")
-assert type(pred) is list
-assert len(pred) == 4
-assert type(pred[0]) is float
+    import ML_predictor_backend as ML
+    pred = ML.predict_next_N_timesteps([1, 2, 3, 4, 5, 6], 2, 4, "linear")
+    assert type(pred) is list
+    assert len(pred) == 4
+    assert type(pred[0]) is float
