91cb2f3fce897eba6b2a2608f29dcfeed09c88fc
---- <56278570+1103s@users.noreply.github.com>
2022-03-11 18:10:14 -0700

    cleaned up some of the doc stuff

--- auto_doc.py
+++ auto_doc.py
@@ -1,21 +1,24 @@
 """
 Crypto-Util Sub-Module Documentation
------------------------------------
-
+===================================
 
 get_data.py
-^^^^^^^^^^^^^^^^^^^^^
+-----------
 ..  automodule:: get_data
     :members:
 
 ML_predictor_backend.py
-^^^^^^^^^^^^^^^^^^^^^
+-----------------------
 ..  automodule:: ML_predictor_backend
     :members:
 
 utils.py
-^^^^^^^^^^^^^^^^^^^^^
+--------
 ..  automodule:: utils
     :members:
 
+test_all.py
+--------
+..  automodule:: test_all
+    :members:
 """
