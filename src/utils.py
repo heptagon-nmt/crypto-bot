354b357184b9e3cbea89d2abe58486dd1771b820
---- <56278570+1103s@users.noreply.github.com>
2022-04-27 20:25:17 -0600

    changed aset type

--- src/utils.py
+++ src/utils.py
@@ -49,7 +49,7 @@ def plot_and_save_price_graph(data, filename, file_extension, crypto):
 	"""
 	data = list(data)
 	assert file_extension in ["pdf", "png", "jpg"], "Supported file extensions are pdf, png and jpg"
-	assert type(filename) is str
+	assert isinstance(filename, Path)
 	assert type(file_extension) is str
 	assert type(data) is list
 	assert type(crypto) is str
