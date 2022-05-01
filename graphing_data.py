0c0cfa268764c5da75beba1909a1293fc5324476
---- <56278570+1103s@users.noreply.github.com>
2022-05-01 13:15:15 -0600

    added in a up/down accuracy rate calculation.

--- graphing_data.py
+++ graphing_data.py
@@ -11,6 +11,7 @@ import time
 from pathlib import Path
 import pickle
 from typing import Tuple, List, Dict
+import itertools as it
 
 MODELS = ['random_forest', 'linear',
           'lasso', 'gradient_boosting', 'bagging', 'ridge']
@@ -35,7 +36,7 @@ def run_sim(days: int,
     theoretic_prices = {}
     for m in MODELS:
         theoretic_prices[m] = yacu.Coin(coin_name,
-                "kraken", days, m).predicted_prices
+                "kraken", days, m, 500).predicted_prices
     return (real_prices, theoretic_prices)
 
 def plot_comp_chart(real: np.ndarray,
@@ -54,9 +55,19 @@ def plot_comp_chart(real: np.ndarray,
         real = real[:len(days)]
     plt.plot(days, np.ndarray.flatten(real),'ro',
             days, predicted, 'bo')
+    r_slope = list(it.pairwise(np.ndarray.flatten(real)))
+    p_slope = list(it.pairwise(predicted))
+    get_slope = lambda x: x[1] - x[0]
+    p_slope = list(map(get_slope, p_slope))
+    r_slope = list(map(get_slope, r_slope))
+    get_pos = lambda x: 1 if (x >= 0) else 0
+    p_slope = list(map(get_pos, p_slope))
+    r_slope = list(map(get_pos, r_slope))
+    matches = [1 if (a and b) else 0 for a, b in zip(p_slope, r_slope)]
+    ud_rate = (sum(matches) * 100) / len(matches)
     plt.xlabel("Days")
     plt.ylabel("Market Price (USD)")
-    plt.title(f'Accuracy of {prediction_type}.')
+    plt.title(f'Accuracy of {prediction_type}. ({ud_rate=}%)')
     real_patch = pat.Patch(color='red', label='Actual Market Price.')
     predicted_patch = pat.Patch(color='blue', label='YACU Predicted Price.')
     ax.legend(handles=[real_patch, predicted_patch])
@@ -70,7 +81,7 @@ if __name__ == "__main__":
         with open(Path("./cache_predicted.pickle"), 'rb') as f:
             predicted = pickle.load(f)
     except Exception as e:
-        real, predicted = run_sim(7, 'BTC')
+        real, predicted = run_sim(14, 'BTC')
         with open(Path("./cache_real.pickle"), 'wb') as f:
             pickle.dump(real, f)
         with open(Path("./cache_predicted.pickle"), 'wb') as f:
